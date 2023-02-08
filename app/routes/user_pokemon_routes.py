e658c7ae1db6e6089be3e1d9cb77c7fa45c72bf1
Yael <yaelasolomon@gmail.com>
2023-02-08 12:43:49 -0800

    Applies auth flow to user_pokemon routes

--- app/routes/user_pokemon_routes.py
+++ app/routes/user_pokemon_routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint, jsonify, abort, make_response, request
+from app.models.pokemon import Pokemon
 from app.models.user_pokemon import UserPokemon
 from app.models.user import User
 from app.utils import get_firebase_user_id, get_user_profile_from_auth_token, validate_model
@@ -9,14 +10,20 @@ user_pokemon_bp = Blueprint("user_pokemon", __name__, url_prefix="/user_pokemon"
 @user_pokemon_bp.route("", methods=["POST"])
 @firebase.jwt_required
 def create_user_pokemon():
-    request_body = request.get_json()
-    if not "user_id" in request_body or not "pokemon_id" in request_body:
-        return make_response({"details":"Invalid submission field; missing title or description"}, 400)
+    query_param = request.args.get('pokemon_id')
 
-    user = get_user_profile_from_auth_token(request.headers["Authorization"])
+    if not query_param:
+        return make_response({"details":"Invalid submission field; missing Pokemon ID"}, 400)
 
-    user_pokemon_request_obj = request_body.copy()
+    firebase_user_id = get_firebase_user_id(
+        get_user_profile_from_auth_token(request.headers["Authorization"])
+    )
+
+    user = User.query.filter(User.firebase_id == firebase_user_id).one_or_none()
+
+    user_pokemon_request_obj = {}
     user_pokemon_request_obj["user_id"] = user.id
+    user_pokemon_request_obj["pokemon_id"] = query_param
 
     new_user_pokemon = UserPokemon.from_dict(user_pokemon_request_obj)
 
@@ -28,7 +35,12 @@ def create_user_pokemon():
 @user_pokemon_bp.route("", methods=["GET"])
 @firebase.jwt_required
 def get_all_user_pokemon():
-    user = get_user_profile_from_auth_token(request.headers['Authorization'])
+    firebase_user_id = get_firebase_user_id(
+        get_user_profile_from_auth_token(request.headers["Authorization"])
+    )
+
+    user = User.query.filter(User.firebase_id == firebase_user_id).one_or_none()
+
     user_pokemon = UserPokemon.query.filter(UserPokemon.user_id == user.id)
 
     return jsonify([pokemon.to_dict() for pokemon in user_pokemon])
@@ -37,17 +49,27 @@ def get_all_user_pokemon():
 @firebase.jwt_required
 def update_user_pokemon_exp(id):
     request_body = request.get_json()
-    user_pokemon = validate_model(UserPokemon, id)
+    firebase_user_id = get_firebase_user_id(
+        get_user_profile_from_auth_token(request.headers["Authorization"])
+    )
 
-    user_pokemon.update_exp(request_body["exp"])
+    user = User.query.filter(User.firebase_id == firebase_user_id).one_or_none()
+    pokemon = UserPokemon.query.get({"user_id": user.id, "pokemon_id": id})
+
+    pokemon.update_exp(request_body["exp"])
     db.session.commit()
-    return {"user pokemon": user_pokemon.to_dict()}
+    return {"user pokemon": pokemon.to_dict()}
 
 @user_pokemon_bp.route("/<id>/reset_exp", methods=["PATCH"])
 @firebase.jwt_required
 def reset_user_pokemon_exp(id):
-    user_pokemon = validate_model(UserPokemon, id)
+    firebase_user_id = get_firebase_user_id(
+        get_user_profile_from_auth_token(request.headers["Authorization"])
+    )
 
-    user_pokemon.reset_exp()
+    user = User.query.filter(User.firebase_id == firebase_user_id).one_or_none()
+    pokemon = UserPokemon.query.get({"user_id": user.id, "pokemon_id": id})
+
+    pokemon.reset_exp()
     db.session.commit()
-    return {"user pokemon": user_pokemon.to_dict()}
+    return {"user pokemon": pokemon.to_dict()}
